908082ca383e539506e3d7ca2f6d464c3b1ca9dd
IvanMakevic <48869595+asdfmake@users.noreply.github.com>
2025-06-19 23:44:02 +0200

    Added logic to instantiate array of starting positions, and instantiate grid

---
+++ Assets/scripts/GameMaster.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject testCube;
+
+    void Start()
+    {
+        Vector3[,] grid = new Vector3[6, 6];
+        GameObject[,] kocke = new GameObject[6, 6];
+        float x = -2.5f;
+        float y = -2.5f;
+
+        // Podesavanje grid-a pocetnim pozicijama
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                grid[i, j] = new Vector3(x, y, 0);
+                x += 1;
+            }
+            x = -2.5f;
+            y += 1;
+        }
+
+        for (int row = 0; row < 6; row++)
+        {
+            for (int col = 0; col < 6; col++)
+            {
+                GameObject obj = Instantiate(testCube, grid[row, col], Quaternion.identity);
+                kocke[row, col] = obj;
+                // todo dodaj logiku na svaku kocku koja ce da zna na kom se indeksu nalazi ili nesto slicno
+                // todo neka se kocka instancira iznad ekrana, a posle, preko funkcije dobija poziciju na koju treba da ide
+                // todo dodaj svaku kocku na jos jednu duplu listu game objekata
+            }
+            
+        }
+    }
+
+    void CheckMatch()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
